ae6bb313800edecdc3bbb14446aeff914359792e
Willian Magana <wamagana@ssf.gob.sv>
2022-01-07 16:02:53 -0600

    Add GameStyle property

--- Tailspin.SpaceGame.Web.Models/Models/Score.cs
+++ Tailspin.SpaceGame.Web.Models/Models/Score.cs
@@ -19,5 +19,8 @@ namespace TailSpin.SpaceGame.Web.Models
         // The game region (map) the score is associated with.
         [JsonPropertyName("gameRegion")]
         public string GameRegion { get; set; }
+
+        [JsonPropertyName("gameStyle")]
+        public string GameStyle { get; set; }
     }
 }
